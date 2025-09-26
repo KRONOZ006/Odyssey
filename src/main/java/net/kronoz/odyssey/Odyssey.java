3d823129e157bf2da9204d064bf66cdc8916e981
KRONOZ006 <therealkronozgmaing@gmail.com>
2025-09-26 15:48:30 -0700

    Hello World

---
+++ src/main/java/net/kronoz/odyssey/Odyssey.java
@@ -0,0 +1,10 @@
+package net.kronoz.odyssey;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Odyssey implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/kronoz/odyssey/client/OdysseyClient.java
@@ -0,0 +1,10 @@
+package net.kronoz.odyssey.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OdysseyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
